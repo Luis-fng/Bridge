daf76c79968f6e74a9b18e5636df76cb82507265
Luís Fernando Nogueira Goffredo <luisfngoffredo@gmail.com>
2026-03-31 09:25:33 -0300

    Add unit tests for TV and Radio controls
    Added tests for TV and Radio functionalities including power on, volume increase, and power off.

--- bridge_teste.py
+++ bridge_teste.py
@@ -1,1 +1,28 @@
+from bridge import TV, Radio, ControleRemoto, ControleAvancado
 
+def test_tv_ligar(capsys):
+    tv = TV()
+    controle = ControleRemoto(tv)
+
+    controle.ligar()
+
+    captured = capsys.readouterr()
+    assert "TV Ligada" in captured.out
+
+def test_radio_volume(capsys):
+    radio = Radio()
+    controle = ControleAvancado(radio)
+
+    controle.aumentar_volume()
+
+    captured = capsys.readouterr()
+    assert "Volume do Rádio: 20" in captured.out
+
+def test_desligar_tv(capsys):
+    tv = TV()
+    controle = ControleRemoto(tv)
+
+    controle.desligar()
+
+    captuerd = capsys.readouterr()
+    assert "TV desligada" in captured.out
